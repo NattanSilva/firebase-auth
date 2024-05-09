537e986e8f3e3af7b6cd117a1b9e5de665b725ac
NattanSilva <natanxboy@gmail.com>
2024-05-09 09:35:47 -0300

    :arrow_up: add: aut validations, routes permissions

--- project/api/fyrebase.py
+++ project/api/fyrebase.py
@@ -1,14 +1,14 @@
 import pyrebase
 
-firebaseConfig = {
-    "apiKey": "AIzaSyAHUP96N1jxm7MfAhrqK3ExxAVS6lqAlYA",
-    "authDomain": "pyrebase-auth.firebaseapp.com",
-    "projectId": "pyrebase-auth",
-    "storageBucket": "pyrebase-auth.appspot.com",
-    "messagingSenderId": "278748329803",
-    "appId": "1:278748329803:web:65aa8082b970ba2f2e5ace",
-    "databaseURL": "https://pyrebase-auth-default-rtdb.firebaseio.com/",
-}
+# firebaseConfig = {
+#     "apiKey": "AIzaSyAHUP96N1jxm7MfAhrqK3ExxAVS6lqAlYA",
+#     "authDomain": "pyrebase-auth.firebaseapp.com",
+#     "projectId": "pyrebase-auth",
+#     "storageBucket": "pyrebase-auth.appspot.com",
+#     "messagingSenderId": "278748329803",
+#     "appId": "1:278748329803:web:65aa8082b970ba2f2e5ace",
+#     "databaseURL": "https://pyrebase-auth-default-rtdb.firebaseio.com/",
+# }
 
 firebase = pyrebase.initialize_app(config=firebaseConfig)
 
